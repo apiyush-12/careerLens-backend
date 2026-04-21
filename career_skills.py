635496b74bf0fb7fb06a4bbc8d3be31767d95d8d
apiyush-12 <pk6990@srmist.edu.in>
2026-04-21 18:10:07 +0530

    backend

---
+++ career_skills.py
@@ -0,0 +1,37 @@
+CAREER_SKILLS = {
+    "data scientist": [
+        "Python", "Pandas", "NumPy", "Machine Learning",
+        "Statistics", "Data Visualization", "SQL"
+    ],
+
+    "frontend developer": [
+        "HTML", "CSS", "JavaScript", "React",
+        "Next.js", "Git"
+    ],
+
+    "backend developer": [
+        "Node.js", "Express", "Databases",
+        "REST API", "Authentication"
+    ],
+
+    "full stack developer": [
+        "HTML", "CSS", "JavaScript",
+        "React", "Node.js", "Databases"
+    ],
+
+    "ai engineer": [
+        "Python", "Machine Learning",
+        "Deep Learning", "TensorFlow", "PyTorch"
+    ],
+
+    "ml engineer": [
+        "Python", "Machine Learning",
+        "Scikit-learn", "Model Deployment",
+        "MLOps"
+    ],
+
+    "devops engineer": [
+        "Linux", "Docker", "Kubernetes",
+        "CI/CD", "AWS"
+    ]
+}
